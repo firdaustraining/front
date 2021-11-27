1eaff3e24d6d08542ca1bd0f67e9771c00ea5914
Faza <firdausauliafaza@gmail.com>
2021-11-27 15:51:34 +0700

    Update api link

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Api = axios.create({
   //set default endpoint API
-  baseURL: "http://backend-shop.test/api",
+  baseURL: "http://solobudaya.web.id/api",
   // baseURL: "http://127.0.0.1:8000/api",
 });
 
