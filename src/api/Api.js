a7d67df2a755b4acec843d1e1a21c9f11616a340
Faza <firdausauliafaza@gmail.com>
2021-11-27 15:58:01 +0700

    api 2

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const Api = axios.create({
   //set default endpoint API
-  baseURL: "http://solobudaya.web.id/api",
+  baseURL: "https://solobudaya.web.id/api",
   // baseURL: "http://127.0.0.1:8000/api",
 });
 
